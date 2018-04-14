334100597bc2934a1175fb8e7745fe11ab518d08
dextop123 <dextop123@gmail.com>
2018-04-14 18:40:07 -0300

    Exemplo de uso de enumeradores

---
+++ UsaEstadosDoBr.java
@@ -0,0 +1,12 @@
+public class UsaEstadosDoBr{
+
+	public static void main (String[] args){
+		
+		EstadosDoBr estado = EstadosDoBr.AMAZONAS;
+		System.out.println(estado.qualArea());
+
+		System.out.println(EstadosDoBr.AMAPA);
+
+	}
+
+}
